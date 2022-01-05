30eb3d334f5a29f762d4de21e3507db02a95b548
W1nterFr3ak <byronchris25@gmail.com>
2022-01-05 19:12:48 +0300

    the correct version

--- install.py
+++ install.py
@@ -2,7 +2,7 @@ import os
 
 if os.path.isfile('requirements.txt'):
     os.system('pip3 install -r requirements.txt')
-    od.system('sudo apt-get install python3-tk')
+    os.system('sudo apt-get install python3-tk')
     os.system("sudo apt install wkhtmltopdf")
 else:
     print ("File not exist, make sure requirements.txt exist in the cwd")
